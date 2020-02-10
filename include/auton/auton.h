c3f48cc1602441942552d329f019794e122b37fa
JavierO <javier.ortiz13@upr.edu>
2020-02-10 02:10:35 -0400

    added ultrasonic functionality with threads for the dual ultrasonic usage

--- include/auton/auton.h
+++ include/auton/auton.h
@@ -29,6 +29,15 @@ namespace auton {
     //moves motor group in same dir stopping on a switch
     void move_group_for_bumper(vex::triport::port &sensor_port, vex::motor_group left_mg, vex::motor_group right_mg, double speed, vex::velocityUnits vel_units);
     
+    //moves motor group in same dir stopping on a distance (sonar ports need to be connected one next to the other)
+    void move_group_for_sonar(vex::triport::port &sensor_port, vex::motor_group mg, double lim, vex::distanceUnits units, double speed, vex::velocityUnits vel_units);
+
+    //moves motor group in same dir stopping on a distance (sonar ports need to be connected one next to the other)
+    void move_group_for_sonar(vex::triport::port &sensor_port, vex::motor_group left_mg, vex::motor_group right_mg, double lim, vex::distanceUnits units, double speed, vex::velocityUnits vel_units);
+
+    //moves motor group in same dir stopping on a distance (sonar ports need to be connected one next to the other)
+    void move_group_for_dual_sonar(vex::triport::port &sensor_port, vex::triport::port &sensor_port_2, vex::motor_group left_mg, vex::motor_group right_mg, double lim, vex::distanceUnits units, double speed, vex::velocityUnits vel_units);
+    
     // Moves motor groups in separete directions, speed sign dictates direction
     void mech_rotate(vex::motor_group left_mg, vex::motor_group right_mg, double lim, vex::rotationUnits rot_units, double speed, vex::velocityUnits vel_units);
     
--- src/auton/auton.cpp
+++ src/auton/auton.cpp
@@ -90,6 +90,85 @@ void Auton::move_group_for_bumper(vex::triport::port &sensor_port,
   group_stop(left_mg, right_mg);
 }
 
+// moves motor group in same dir stopping on a distance (sonar ports need to be
+// connected one next to the other)
+void Auton::move_group_for_sonar(vex::triport::port &sensor_port,
+                                 vex::motor_group mg, double lim,
+                                 vex::distanceUnits units, double speed,
+                                 vex::velocityUnits vel_units) {
+  vex::sonar sensor = vex::sonar(sensor_port);
+
+  while (sensor.distance(units) < lim) {
+    // while(1){
+    Brain.Screen.setCursor(1, 1);
+    Brain.Screen.print(sensor.value());
+
+    move_group(mg, speed, vel_units);
+
+    task::sleep(5);
+  }
+
+  group_stop(mg, mg);
+}
+
+// moves motor group in same dir stopping on a distance (sonar ports need to be
+// connected one next to the other)
+void Auton::move_group_for_sonar(vex::triport::port &sensor_port,
+                                 vex::motor_group left_mg,
+                                 vex::motor_group right_mg, double lim,
+                                 vex::distanceUnits units, double speed,
+                                 vex::velocityUnits vel_units) {
+  vex::sonar sensor = vex::sonar(sensor_port);
+
+  while (sensor.distance(units) < lim) {
+    // while(1){
+    Brain.Screen.setCursor(1, 1);
+    Brain.Screen.print(sensor.value());
+
+    move_group(left_mg, speed, vel_units);
+    move_group(right_mg, speed, vel_units);
+
+    task::sleep(5);
+  }
+
+  group_stop(left_mg, right_mg);
+}
+
+// moves motor group in same dir stopping on a distance (sonar ports need to be
+// connected one next to the other)
+void Auton::move_group_for_dual_sonar(vex::triport::port &sensor_port,
+                                      vex::triport::port &sensor_port_2,
+                                      vex::motor_group left_mg,
+                                      vex::motor_group right_mg, double lim,
+                                      vex::distanceUnits units, double speed,
+                                      vex::velocityUnits vel_units) {
+
+  // thread's callback function cannot have any parameters
+  // a capturing lambda cannot convert to void type
+  // a non capturing lambda can convert to void type
+  // The gist of the trick is passing the lambda function as the callback
+  // argument and providing a captureless thunk as the callback function pointer
+
+  auto left = [this, &sensor_port, left_mg, lim, units, speed, vel_units]() {
+    move_group_for_sonar(sensor_port, left_mg, lim, units, speed, vel_units);
+  };
+  auto left_thunk = [](void *arg) { // note thunk is captureless
+    (*static_cast<decltype(left) *>(arg))();
+  };
+
+  auto right = [this, &sensor_port_2, right_mg, lim, units, speed,
+                vel_units]() {
+    move_group_for_sonar(sensor_port_2, right_mg, lim, units, speed, vel_units);
+  };
+  auto right_thunk = [](void *arg) { // note thunk is captureless
+    (*static_cast<decltype(right) *>(arg))();
+  };
+
+  // start threads
+  vex::thread t(left_thunk, &left);
+  vex::thread t_R(right_thunk, &right);
+}
+
 // Moves motor groups in separete directions, speed sing dictates direction
 void Auton::mech_rotate(vex::motor_group left_mg, vex::motor_group right_mg,
                         double lim, vex::rotationUnits rot_units, double speed,
@@ -108,8 +187,7 @@ void Auton::mech_rotate(vex::motor_group left_mg, vex::motor_group right_mg,
 
 void Auton::mech_rotate_gyro(vex::triport::port &sensor_port,
                              vex::motor_group left_mg,
-                             vex::motor_group right_mg, 
-                             double deg,
+                             vex::motor_group right_mg, double deg,
                              double speed, vex::velocityUnits vel_units) {
 
   gyro sensor = gyro(sensor_port);
@@ -118,7 +196,7 @@ void Auton::mech_rotate_gyro(vex::triport::port &sensor_port,
   sensor.resetHeading();
   // sensor.setHeading(0.0, rot_units);
 
-  if (deg < 0) {//ccw
+  if (deg < 0) { // ccw
     deg = 360 + deg;
     sensor.setHeading(359.0, rotationUnits::deg);
 
@@ -131,7 +209,7 @@ void Auton::mech_rotate_gyro(vex::triport::port &sensor_port,
 
       task::sleep(5);
     }
-  } else {//cw
+  } else { // cw
     // if(rot_units == rotationUnits::deg) sensor.setHeading(359.0, rot_units);
     while (sensor.heading(rotationUnits::deg) <= deg) {
       move_group(left_mg, speed, vel_units);
@@ -146,9 +224,6 @@ void Auton::mech_rotate_gyro(vex::triport::port &sensor_port,
 
   group_stop(left_mg, right_mg, brakeType::hold);
 }
-
-// TODO define remaining methods from header file
-//      Most of these methods can reuse the definitions from old_code.cpp
 
 // Stops motor groups, depending on brake type can be used for base or arm
 void Auton::group_stop(vex::motor_group left_mg, vex::motor_group right_mg,
